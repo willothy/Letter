d26785debef99b3aea0b0760633ea4a2525c608a
Will Hopkins <38540736+willothy@users.noreply.github.com>
2022-04-05 03:07:46 -0700

    Added Evaluator.js outline

---
+++ src/Evaluator.js
@@ -0,0 +1,19 @@
+
+
+class Evaluator {
+    /**
+     * @constructor
+     */
+    constructor() {
+        this._ast = {};
+    }
+
+    /**
+     * Evaluates a given expression or program
+     * @param {ASTNode} node 
+     * @returns {void}
+     */
+    eval(node) {
+        
+    }
+}
